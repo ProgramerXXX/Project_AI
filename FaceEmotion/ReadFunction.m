3562a452be4e599c88f000d0c44cf4021cc8cdaf
Teera <84917837+ProgramerXXX@users.noreply.github.com>
2022-04-14 18:10:04 +0700

    Delete FaceEmotion directory

--- FaceEmotion/ReadFunction.m
+++
@@ -1,7 +0,0 @@
-% Copyright 2017 The MathWorks, Inc.
-
-function I = readFunctionTrain('man.jpg')
-% Resize the flowers images to the size required by the network.
-I = imread(filename);
-
-I = imresize(I, [227 227]);
